a79d651af672c449d5710b2254e352dca3a6a480
carlogattuso <carlogc46@gmail.com>
2019-10-04 16:10:33 +0200

    first commit

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,6 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {Router} from "@angular/router";
+import {Component, Injectable, NgModule, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import {Observable, pipe} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    // Include it under 'imports' in your application module
+    // after BrowserModule.
+    HttpClientModule,
+  ],
+})
+
+@Injectable()
+export class ConfigService {
+  constructor(private http: HttpClient) { }
+}
 
 @Component({
   selector: 'app-login',
@@ -18,19 +37,22 @@ export class LoginComponent implements OnInit {
   };
   public loginForm: FormGroup;
 
+  // tslint:disable-next-line:variable-name
   validation_messages = {
-    'email': [
+    email: [
       { type: 'required', message: 'Email is required.' },
       { type: 'pattern', message: 'Email is not valid.' }
     ],
-    'password': [
+    password: [
       { type: 'required', message: 'Password is required.' },
       { type: 'minlength', message: 'Password must be at least 5 characters long.' },
       { type: 'maxlength', message: 'Password cannot be more than 25 characters long.' },
     ],
   };
+  private http: any;
+
   constructor(public formBuilder: FormBuilder,
-              public router: Router,) {
+              public router: Router, ) {
     this.loginForm = formBuilder.group({
       email: ['',  Validators.compose([
         Validators.required,
@@ -47,7 +69,39 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
-  async login(){
+  async login() {
     console.log(this.user);
+
+    const body = '{\n' +
+      '\t"email":"carlogc46@gmail.com",\n' +
+      '\t"password":"1234"\n' +
+      '}';
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        Authorization: 'my-auth-token'
+      })
+    };
+
+    console.log(body);
+
+    const res = this.http.post('localhost:3000/user/login', body, httpOptions);
+
+    // New branch created
   }
+    /*const headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+    this.http
+      .post('localhost:3000/test',
+          body, {
+            headers
+          })
+        .subscribe(data => {
+          alert('ok');
+        }, error => {
+          console.log(JSON.stringify(error.json()));
+        });
+     */
 }
