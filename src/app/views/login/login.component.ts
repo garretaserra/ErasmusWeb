1a64b64a88232564f0a7b71d63dde3ddcc406940
sheilalopez <47568080+sheilalopez@users.noreply.github.com>
2019-11-18 15:05:26 +0100

    primeros retoques

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -19,18 +19,18 @@ export class LoginComponent implements OnInit {
   public loginForm: FormGroup;
 
   validation_messages = {
-    'email': [
+    email: [
       { type: 'required', message: 'Email is required.' },
       { type: 'pattern', message: 'Email is not valid.' }
     ],
-    'password': [
+    password: [
       { type: 'required', message: 'Password is required.' },
       { type: 'minlength', message: 'Password must be at least 5 characters long.' },
       { type: 'maxlength', message: 'Password cannot be more than 25 characters long.' },
     ],
   };
   constructor(public formBuilder: FormBuilder,
-              public router: Router,) {
+              public router: Router, ) {
     this.loginForm = formBuilder.group({
       email: ['',  Validators.compose([
         Validators.required,
@@ -47,7 +47,7 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
-  async login(){
+  async login() {
     console.log(this.user);
   }
 }
