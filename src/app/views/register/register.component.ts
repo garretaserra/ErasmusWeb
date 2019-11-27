02272b586640e3f5201beceab1ece7d2d4783410
sheilalopez <47568080+sheilalopez@users.noreply.github.com>
2019-11-27 19:45:39 +0100

    starting with regiter page

--- src/app/views/register/register.component.ts
+++ src/app/views/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -9,7 +10,9 @@ export class RegisterComponent implements OnInit {
 
   constructor() { }
 
+
   ngOnInit() {
   }
 
 }
+
